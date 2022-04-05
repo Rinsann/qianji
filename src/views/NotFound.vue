02df112ff8fdad336af7724b1efeabb05e65a690
yume <785879986@qq.com>
2022-04-05 13:35:05 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="error">
+    <div>404,页面没有找到</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NotFound"
+};
+</script>
+
+<style lang="scss" scoped>
+.error {
+  font-size: 60px;
+  color: gold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  a {
+
+  }
+}
+</style>
