f61242c0f2a7d69d3308eae101fce0a5c6cbfc24
yume <785879986@qq.com>
2022-04-05 13:56:58 +0800

    创建 <Layout/> 使用 插槽 slot

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      Labels.vue
-    </div>
-    <Nav/>
+  <div>
+    <Layout>
+      <p>Labels.vue</p>
+    </Layout>
   </div>
 </template>
 
@@ -14,16 +13,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid green;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
 
-  .content {
-    border: 1px solid blue;
-    overflow: auto;
-    flex-grow: 1;
-  }
-}
 </style>
