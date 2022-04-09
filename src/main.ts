004eb2cb2b330851a5bae341d67af4ea803a0bfc
yume <785879986@qq.com>
2022-04-09 22:17:12 +0800

    将所有的数据操作封装到 store 中

--- src/main.ts
+++ src/main.ts
@@ -14,30 +14,8 @@ Vue.component("Nav", Nav);
 Vue.component("Layout", Layout);
 Vue.component("Icon", Icon);
 
-// record store
-window.recordList = recordListModel.fetch();
-window.createRecord = (record: RecordItem) => recordListModel.create(record);
 
-// tag store
-window.findTag = (id: string) => {
-  return window.tagList.filter(t => t.id === id)[0];
-};
-window.tagList = tagListModel.fetch();
-window.createTag = (name: string) => {
-  tagListModel.create(name);
-  if (name) {
-    const message = tagListModel.create(name);
-    if (message === "duplicated") {
-      window.alert("标签名重复");
-    }
-  }
-};
-window.removeTag = (id: string) => {
-  return tagListModel.remove(id);
-};
-window.updateTag = (id: string, name: string) => {
-  return tagListModel.update(id, name);
-};
+
 
 new Vue({
   router,
