4a3a6f9a15a2148b38e660f380e0b61d1f1443e6
yume <785879986@qq.com>
2022-04-09 10:39:37 +0800

    封装model

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
     <Layout>
-      <p>Labels.vue</p>
+      <ol>
+        <li>衣
+          <Icon name="right"/>
+        </li>
+        <li>食
+          <Icon name="right"/>
+        </li>
+        <li>住
+          <Icon name="right"/>
+        </li>
+        <li>行
+          <Icon name="right"/>
+        </li>
+      </ol>
+      <div>
+        <button>新建标签</button>
+      </div>
     </Layout>
   </div>
 </template>
