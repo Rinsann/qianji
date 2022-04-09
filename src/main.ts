0340969d1f0290415d197f4e801b24c5308fc0ef
yume <785879986@qq.com>
2022-04-09 21:53:38 +0800

    封装 record 的操作

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,18 @@ import Nav from "@/components/Nav.vue";
 import Layout from "@/components/Layout.vue";
 import Icon from "@/components/Icon.vue";
 import tagListModel from "@/models/tagListModel";
+import recordListModel from "@/models/recordListModel";
 
 Vue.config.productionTip = false;
 Vue.component("Nav", Nav);
 Vue.component("Layout", Layout);
 Vue.component("Icon", Icon);
 
+// record store
+window.recordList = recordListModel.fetch();
+window.createRecord = (record: RecordItem) => recordListModel.create(record);
+
+// tag store
 window.findTag = (id: string) => {
   return window.tagList.filter(t => t.id === id)[0];
 };
@@ -26,11 +32,9 @@ window.createTag = (name: string) => {
     }
   }
 };
-
 window.removeTag = (id: string) => {
   return tagListModel.remove(id);
 };
-
 window.updateTag = (id: string, name: string) => {
   return tagListModel.update(id, name);
 };
