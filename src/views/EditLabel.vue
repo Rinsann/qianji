c117c2899add9346de0d239e66e90943cf942eee
yume <785879986@qq.com>
2022-04-10 14:28:08 +0800

    重构 EditLabel.vue

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -23,13 +23,17 @@ import FormItem from "@/components/Money/FormItem.vue";
 import Button from "@/components/Button.vue";
 
 @Component({
-  components: {Button, FormItem}
+  components: {Button, FormItem},
+
 })
 export default class EditLabel extends Vue {
-  tag?: Tag = undefined;
+  get tag() {
+    return this.$store.state.currentTag;
+  }
 
   created() {
-    this.tag = undefined/*  store.findTag(this.$route.params.id); */
+    const id = this.$route.params.id;
+    this.$store.commit("setCurrentTag", id);
     if (!this.tag) {
       this.$router.replace("/404");
     }
@@ -43,13 +47,13 @@ export default class EditLabel extends Vue {
   }
 
   remove() {
-  /*   if (this.tag) {
-      if (store.removeTag(this.tag.id)) {
-        this.$router.back();
-      } else {
-        window.alert("删除失败");
-      }
-    } */
+    /*   if (this.tag) {
+        if (store.removeTag(this.tag.id)) {
+          this.$router.back();
+        } else {
+          window.alert("删除失败");
+        }
+      } */
   }
 
   goBack() {
