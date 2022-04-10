e9269e2cf01639fc28b467aed4bfd8c20fd0c352
yume <785879986@qq.com>
2022-04-10 17:55:35 +0800

    使用 Tabs.vue 替代 Types.vue,删除 Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,7 @@
   <div>
     <Layout class-prefix="layout">
       <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
-      <Types :value.sync="record.type"/>
+      <Tabs :data-source="recordTypeList" :value.sync="record.type"/>
       <div class="notes">
         <FormItem @update:value="onUpdateNotes" field-name="备注" placeholder="请输入备注"/>
       </div>
@@ -15,16 +15,21 @@
 
 import Vue from "vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
-import Types from "@/components/Money/Types.vue";
 import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
 import {Component} from "vue-property-decorator";
+import Tabs from "@/components/Tabs.vue";
+import recordTypeList from "@/constants/recordTypeList";
+import intervalList from "@/constants/intervalList";
 
 @Component({
-  components: {Tags, FormItem, Types, NumberPad},
+  components: {Tabs, Tags, FormItem, NumberPad},
 })
 export default class Money extends Vue {
   record: RecordItem = {tags: [], notes: "", type: "-", amount: 0};
+
+  recordTypeList = recordTypeList;
+  intervalList = intervalList;
 
   get recordList() {
     return this.$store.state.recordList;
