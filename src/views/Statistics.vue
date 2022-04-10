896b44e440c33c3edb5485646392a869536b8b79
yume <785879986@qq.com>
2022-04-10 18:05:36 +0800

    消除多余的 ::v-deep

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
+    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval" />
     <div>
       type：{{ type }} <br>
       interval:{{ interval }}
@@ -29,19 +29,21 @@ export default class Statistics extends Vue {
 </script>
 
 <style lang="scss" scoped>
-::v-deep .type-tabs-item {
-  background: white;
+::v-deep {
+  .type-tabs-item {
+    background: white;
 
-  &.selected {
-    background: #c4c4c4;
+    &.selected {
+      background: #c4c4c4;
 
-    &::after {
-      display: none;
+      &::after {
+        display: none;
+      }
     }
   }
-}
 
-::v-deep .interval-tabs-item {
-  height: 48px;
+  .interval-tabs-item {
+    height: 48px;
+  }
 }
 </style>
