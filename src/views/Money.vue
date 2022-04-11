0bd003c709f8dd4ce7a1cafbabab552fd1cb70cb
yume <785879986@qq.com>
2022-04-11 22:54:30 +0800

    修复 Money.vue 样式问题

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,32 +1,34 @@
 <template>
   <div>
     <Layout class-prefix="layout">
+
       <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
       <Tabs :data-source="recordTypeList" :value.sync="record.type"/>
       <div class="notes">
         <FormItem @update:value="onUpdateNotes" field-name="备注" placeholder="请输入备注"/>
       </div>
       <Tags/>
+
     </Layout>
   </div>
 </template>
 
 <script lang="ts">
 
-import Vue from "vue";
-import NumberPad from "@/components/Money/NumberPad.vue";
-import FormItem from "@/components/Money/FormItem.vue";
-import Tags from "@/components/Money/Tags.vue";
-import {Component} from "vue-property-decorator";
-import Tabs from "@/components/Tabs.vue";
-import recordTypeList from "@/constants/recordTypeList";
-import intervalList from "@/constants/intervalList";
+import Vue from 'vue';
+import NumberPad from '@/components/Money/NumberPad.vue';
+import FormItem from '@/components/Money/FormItem.vue';
+import Tags from '@/components/Money/Tags.vue';
+import {Component} from 'vue-property-decorator';
+import Tabs from '@/components/Tabs.vue';
+import recordTypeList from '@/constants/recordTypeList';
+import intervalList from '@/constants/intervalList';
 
 @Component({
   components: {Tabs, Tags, FormItem, NumberPad},
 })
 export default class Money extends Vue {
-  record: RecordItem = {tags: [], notes: "", type: "-", amount: 0};
+  record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
   recordTypeList = recordTypeList;
   intervalList = intervalList;
@@ -36,7 +38,7 @@ export default class Money extends Vue {
   }
 
   created() {
-    this.$store.commit("fetchRecords");
+    this.$store.commit('fetchRecords');
   }
 
   onUpdateNotes(value: string) {
@@ -44,13 +46,13 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    this.$store.commit("createRecord", this.record);
+    this.$store.commit('createRecord', this.record);
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.layout-content {
+::v-deep .layout-content {
   display: flex;
   flex-direction: column-reverse;
 }
